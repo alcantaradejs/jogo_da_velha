d6277c13f39ffb4f3418f46571717d83c50aa9ce
Isaque Alcantara <alcantara.isaque167@gmail.com>
2022-05-10 18:21:55 +0000

    title created

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { Board } from "./components/board/Board"
 function App() {
 
   return (
-    <div className="flex w-[100vw] h-[100vh] justify-center items-center">
-      <Board/>
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center">
+        <h1 className="text-3xl font-bold font-mono text-center mb-6">Jogo da velha</h1>
+        <Board/>
     </div>
   )
 }
