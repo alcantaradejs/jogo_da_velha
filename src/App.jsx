2ede0a0d6e76e8368920e51f1b550f690cb2a30c
Isaque Alcantara <alcantara.isaque167@gmail.com>
2022-05-10 16:32:51 +0000

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Board } from "./components/board/Board"
+
 function App() {
 
   return (
-    <div className="App">
-      <button>hello Wolrd!!</button>
+    <div className="flex w-[100vw] h-[100vh] justify-center items-center">
+      <Board/>
     </div>
   )
 }
